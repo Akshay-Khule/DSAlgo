39dbe693253c3dbe119dae46e2fa8ab9d095e290
Akshay-Khule <75080370+Akshay-Khule@users.noreply.github.com>
2025-02-27 13:20:13 +0530

    Update FindKClosestElements.java

--- FindKClosestElements.java
+++ FindKClosestElements.java
@@ -7,7 +7,7 @@ public class FindKClosestElements {
 
 	public static void main(String[] args) {
 
-		int[] arr = {1,2,3,4,5};
+		int[] arr = {1,2,3,4,5,6};
 		int k = 4, x = 3;
 		
 		findClosestElements(arr, k, x);
